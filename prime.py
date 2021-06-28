a12c06ec4022045e07967b6d8a5ce33be26fc1b2
loiseupro <86427844+loiseupro@users.noreply.github.com>
2021-06-28 09:51:07 +0200

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,27 @@
+from random import randint
+
+def isPrime(num):
+	cont = 0
+	i = 1
+
+	for i in range(0, num):
+		i+=1
+		if (num%i == 0):
+			cont+=1; 
+
+	if cont == 2:
+		return True;
+	
+	return False;
+
+
+def getPrime():
+   result = False;
+   
+   while result == False:
+    	# You can modify limit to get random prime number
+    	random = randint(0, 1000);
+    	if isPrime(random):
+    		result = random   
+   
+   return result;
